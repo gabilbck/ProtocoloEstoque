e662b2a6d85d09cad201fe64710e433ad01842c9
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-11-15 20:25:45 -0300

    Update testeServer.py

--- testeServer.py
+++ testeServer.py
@@ -50,7 +50,7 @@ def servidor():
 (_)-(_)
 """
         conexao.send(f"Papai Noel: Parabéns! Você foi uma boa criança! Aqui está seu presente:\n{presente}".encode())
-        print("Conversa encerrada: Criança foi aprovada e recebeu o presente.")
+        print("Conversa encerrada: Criança fez bem e recebeu o presente.")
         
         # Após enviar o presente, fechamos a conexão e não aceitamos mais respostas
         conexao.close()
