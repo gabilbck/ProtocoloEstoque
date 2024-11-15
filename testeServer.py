52c20b50b488d6b5022f3a89dd2f87373121f5f5
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-11-15 20:10:18 -0300

    Update testeServer.py

--- testeServer.py
+++ testeServer.py
@@ -16,8 +16,17 @@ def servidor():
     perguntas = [
         "Você foi uma boa criança este ano? (sim/não)",
         "Você ajudou nas tarefas de casa? (sim/não)",
-        "Você foi gentil com seus amigos? (sim/não)"
+        "Você foi gentil com seus amigos? (sim/não)",
+        "Você fez sua lição de casa sem reclamar? (sim/não)",
+        "Você pediu desculpas quando errou? (sim/não)",
+        "Você compartilhou seus brinquedos com outros? (sim/não)",
+        "Você cuidou bem dos seus pets ou plantas? (sim/não)",
+        "Você agradeceu às pessoas que te ajudaram? (sim/não)",
+        "Você não brigou com seus irmãos ou amigos? (sim/não)",
+        "Você escovou os dentes todos os dias? (sim/não)"
     ]
+
+    todas_respostas_sim = True
 
     for pergunta in perguntas:
         conexao.send(pergunta.encode())
@@ -25,15 +34,56 @@ def servidor():
 
         print(f"Criança respondeu: {resposta}")
 
-        if resposta == "não":
+        if resposta != "sim":
+            todas_respostas_sim = False
             conexao.send("Papai Noel: Você foi uma má criança! Vá embora!".encode())
             print("Conversa encerrada: Criança foi mandada embora.")
             conexao.close()
             servidor_socket.close()
             return
 
-    conexao.send("Papai Noel: Parabéns! Você foi uma boa criança! Feliz Natal!".encode())
-    print("Conversa encerrada: Criança foi aprovada.")
+    if todas_respostas_sim:
+        presente = """
+ _________________________________________________________
+|\\=========================================================\\
+||                                                         |
+||        _        __        ___        __        _        |
+||       ; `-.__.-'. `-.__.-'. .`-.__.-' .`-.__.-' :       |
+||     _.'. . . . . . . . .,,,,,,,. . . . . . . . .`._     |
+||   .'. . . . . . . . ,a@@@@@@@@@@@a, . . . . . . . .`.   |
+||   `. . . . ,a@@@@@a@@@a@@@@@@@@@a@@@a@@@@@a, . . . ,'   |
+||     ) . . a@@@@@@a@@@@@a@@@@@@@a@@@@@a@@@@@@a . . (     |
+||   ,' . . .@@@%%%a@@@@@@@@@@@@@@@@@@@@@a%%%@@@  . . `.   |
+||   `.. . . @@@%%a@@@@@@""@@@@@@@""@@@@@@a%%@@@ . . .,'   |
+||     ). . . "@@a@@@@@@@@@SSSSSSS@@@@@@@@@a@@" . . .(     |
+||   ,'. . . . . `@@@@@@@@SSS, ,SSS@@@@@@@@' . . . . .`.   |
+||   `. . . . . . `@@@@@@@`SSS:SSS'@@@@@@@' . . . . . ,'   |
+||     ) . . . . . `@@@@@@@sssssss@@@@@@@' . . . . . (     |
+||   ,' . . . . . ,a@@a@@@@@@@@@@@@@@@a@@a, . . . . . `.   |
+||   `.. . . . .a@@@a@@@@@a@@@a@@@a@@@@@a@@@a. . . . .,'   |
+||     ). . . .a@@@@@a@@@@@@@@@@@@@@@@@a@@@@@a. . . .(     |
+||   ,'. . . . @@@@@@a@@@@'   "   `@@@@a@@@@@@ . . . .`.   |
+||   `. . . . .@@@@@@@aaaa,       ,aaaa@@@@@@@  . . . ,'   |
+||     ) . . . `@@@@@@@@@@@@a, ,a@@@@@@@@@@@@' . . . (     |
+||   ,' . . . . .`@@@@@@@@@@a@a@a@@@@@@@@@@'. . . . . `.   |
+||   `;;;;;;;;;;;;aaaaaaaaaa@@@@@aaaaaaaaaa;;;;;;;;;;;;'   |
+||     );;;;;;;,mMMMMMMMm@@@@@@@@@@@mMMMMMMMm,;;;;;;;(     |
+||   ,;;;;;;;;a@%#%%#%%#%Mm@@@@@@@mM%#%%#%%#%@a;;;;;;;;,   |
+||   `;;;;;;;;@@%%%%%%%%%%M@@";"@@M%%%%%%%%%%@@;;;;;;;;'   |
+||     );;;;;;`@a%%%%%%%%mM";;;;;"Mm%%%%%%%%a@';;;;;;(     |
+||   ,;;;;;;;;;;"@@@@@@@@";;;;;;;;;"@@@@@@@@";;;;;;;;;;,   |
+||   `;;;;;;;;;;;;"""""";;;;;;;;;;;;;"""""";;;;;;;;;;;;'   |
+||     );;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;-Catalyst(     |
+||     `:;;;:-~~~-:;;:-~~~-:;;;;;:-~~~-:;;:,-~~~-:;;;:'    |
+||       ~~~       ~~        ~~~        ~~        ~~~      |
+||                     .=============.                     |
+||                     | Sr. Ursinho |                     |
+||                     `-------------'                     |
+|\\_________________________________________________________|
+"""
+        conexao.send(f"Papai Noel: Parabéns! Você foi uma boa criança! Aqui está seu presente:\n{presente}".encode())
+        print("Conversa encerrada: Criança foi aprovada e recebeu o presente.")
+
     conexao.close()
     servidor_socket.close()
 
